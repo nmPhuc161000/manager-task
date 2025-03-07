2863d1c180897ccfb6671b34fe6d86a34b00a26f
nmPhuc161000 <phucnmse161000@fpt.edu.vn>
2025-03-07 13:17:15 +0700

    add api of login, register, verify email

--- src/apis/url-api.js
+++ src/apis/url-api.js
@@ -1,2 +1,7 @@
-const urlApi = "link here";
-export default urlApi;
+import axios from "axios";
+const apiClient = axios.create({
+    baseURL:
+        "https://localhost:7284",
+    timeout: 100000, // Thời gian timeout 10 giây
+});
+export default apiClient;
