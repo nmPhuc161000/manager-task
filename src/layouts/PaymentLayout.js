97f582cd1d68db9515d8ebb43019a7dedb55eece
loi2003 <loitntse171276@fpt.edu.vn>
2025-04-14 01:45:51 +0700

    add before payment

--- src/layouts/PaymentLayout.js
+++ src/layouts/PaymentLayout.js
@@ -2,16 +2,14 @@ import React from 'react';
 import HeaderDashboard from '../components/header/HeaderDashboard';
 
 const PaymentLayout = ({ children }) => {
-    return (
-        <div className="dashboard-layout">
-            <HeaderDashboard />
-            <div className="layout-container">
-                <div className="content">
-                    {children}
-                </div>
-            </div>
-        </div>
-    );
+  return (
+    <div className="dashboard-layout">
+      <HeaderDashboard />
+      <div className="layout-container">
+        <div className="content">{children}</div>
+      </div>
+    </div>
+  );
 };
 
-export default PaymentLayout; 
+export default PaymentLayout;
